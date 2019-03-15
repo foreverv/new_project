6cd952e1fac478296ccb1869e856cf4dbc0e0574
foreverv <40000638+foreverv@users.noreply.github.com>
2019-03-15 14:14:49 +0800

    Update admin.py

--- new_project/login/admin.py
+++ new_project/login/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from . import models
+from login import models
 
 admin.site.register(models.User)
 admin.site.register(models.ConfirmString)
